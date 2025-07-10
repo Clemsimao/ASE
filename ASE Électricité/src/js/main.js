8b39fff4c59d445375bbc0a821fecfd11d68e2be
Clemsimao <simaoclement33@gmail.com>
2025-07-10 18:35:51 +0200

    add: changing image to the click for the section particulier

--- ASE Électricité/src/js/main.js
+++ ASE Électricité/src/js/main.js
@@ -2,3 +2,15 @@ import '../style/scss/reset.scss'
 import '../style/scss/header.scss'
 import '../style/scss/main.scss'
 import '../style/scss/grid/particulier.scss'
+
+// changement de photos sur un clique 
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.items-body-content span[data-img]').forEach(span => {
+    span.addEventListener('click', function() {
+      const imgSrc = this.getAttribute('data-img');
+      if (imgSrc) {
+        document.getElementById('main-photo').src = './public/images/exemples/' + imgSrc;
+      }
+    });
+  });
+});
