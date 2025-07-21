6b20a9c66b10c1e3edbb4685ebb874edc4c4ac66
Clemsimao <simaoclement33@gmail.com>
2025-07-21 14:43:44 +0200

    feat: photo selection all div

--- ASE Électricité/src/js/main.js
+++ ASE Électricité/src/js/main.js
@@ -3,7 +3,44 @@ import '../style/scss/header.scss'
 import '../style/scss/main.scss'
 import '../style/scss/grid/particulier.scss'
 
-// changement de photos sur un clique 
+/* changement de photos sur un clique div 1
+document.addEventListener('DOMContentLoaded', () => {
+  // Sélectionne tous les blocs cliquables avec data-img
+  const items = document.querySelectorAll('.items-body-content[data-img]');
+  const mainPhoto = document.getElementById('main-photo');
+
+  // Fonction pour mettre à jour la surbrillance selon l'image affichée
+  function updateHighlight(imgName) {
+    items.forEach(item => {
+      if (item.getAttribute('data-img') === imgName) {
+        item.classList.add('highlight');
+      } else {
+        item.classList.remove('highlight');
+      }
+    });
+  }
+
+  // Initialisation : surbrillance sur l'élément correspondant à l'image affichée
+  if (mainPhoto) {
+    // Récupère juste le nom du fichier (sans le chemin)
+    const currentImg = mainPhoto.src.split('/').pop();
+    updateHighlight(currentImg);
+  }
+
+  // Ajout du curseur pointer sur les blocs cliquables
+  items.forEach(item => {
+    item.style.cursor = 'pointer';
+    item.addEventListener('click', function() {
+      const imgSrc = this.getAttribute('data-img');
+      if (imgSrc && mainPhoto) {
+        mainPhoto.src = './public/images/exemples/' + imgSrc;
+        updateHighlight(imgSrc);
+      }
+    });
+  });
+});*/
+
+// changement de photos sur un clique div 3
 document.addEventListener('DOMContentLoaded', () => {
   // Sélectionne tous les blocs cliquables avec data-img
   const items = document.querySelectorAll('.items-body-content[data-img]');
@@ -39,3 +76,77 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+/* changement de photos sur un clique div 5
+document.addEventListener('DOMContentLoaded', () => {
+  // Sélectionne tous les blocs cliquables avec data-img
+  const items = document.querySelectorAll('.items-body-content[data-img]');
+  const mainPhoto = document.getElementById('main-photo');
+
+  // Fonction pour mettre à jour la surbrillance selon l'image affichée
+  function updateHighlight(imgName) {
+    items.forEach(item => {
+      if (item.getAttribute('data-img') === imgName) {
+        item.classList.add('highlight');
+      } else {
+        item.classList.remove('highlight');
+      }
+    });
+  }
+
+  // Initialisation : surbrillance sur l'élément correspondant à l'image affichée
+  if (mainPhoto) {
+    // Récupère juste le nom du fichier (sans le chemin)
+    const currentImg = mainPhoto.src.split('/').pop();
+    updateHighlight(currentImg);
+  }
+
+  // Ajout du curseur pointer sur les blocs cliquables
+  items.forEach(item => {
+    item.style.cursor = 'pointer';
+    item.addEventListener('click', function() {
+      const imgSrc = this.getAttribute('data-img');
+      if (imgSrc && mainPhoto) {
+        mainPhoto.src = './public/images/exemples/' + imgSrc;
+        updateHighlight(imgSrc);
+      }
+    });
+  });
+}); */
+
+/* changement de photos sur un clique div 7
+document.addEventListener('DOMContentLoaded', () => {
+  // Sélectionne tous les blocs cliquables avec data-img
+  const items = document.querySelectorAll('.items-body-content[data-img]');
+  const mainPhoto = document.getElementById('main-photo');
+
+  // Fonction pour mettre à jour la surbrillance selon l'image affichée
+  function updateHighlight(imgName) {
+    items.forEach(item => {
+      if (item.getAttribute('data-img') === imgName) {
+        item.classList.add('highlight');
+      } else {
+        item.classList.remove('highlight');
+      }
+    });
+  }
+
+  // Initialisation : surbrillance sur l'élément correspondant à l'image affichée
+  if (mainPhoto) {
+    // Récupère juste le nom du fichier (sans le chemin)
+    const currentImg = mainPhoto.src.split('/').pop();
+    updateHighlight(currentImg);
+  }
+
+  // Ajout du curseur pointer sur les blocs cliquables
+  items.forEach(item => {
+    item.style.cursor = 'pointer';
+    item.addEventListener('click', function() {
+      const imgSrc = this.getAttribute('data-img');
+      if (imgSrc && mainPhoto) {
+        mainPhoto.src = './public/images/exemples/' + imgSrc;
+        updateHighlight(imgSrc);
+      }
+    });
+  });
+}); */
